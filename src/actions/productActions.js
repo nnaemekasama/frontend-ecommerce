2c3f86b8293a29ec8b72f42e79126c869cc2e050
Nnaemeka Nnodim <mannynnodim@gmail.com>
2023-06-12 12:35:15 +0100

    update

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -1,160 +1,185 @@
-import axios from 'axios'
-import { 
-        PRODUCT_LIST_REQUEST,
-        PRODUCT_LIST_SUCCESS,
-        PRODUCT_LIST_FAIL, 
-        PRODUCT_DETAILS_REQUEST,
-        PRODUCT_DETAILS_SUCCESS,
-        PRODUCT_DETAILS_FAIL, 
-        PRODUCT_DELETE_REQUEST,
-        PRODUCT_DELETE_SUCCESS,
-        PRODUCT_DELETE_FAIL,
-        PRODUCT_CREATE_REQUEST,
-        PRODUCT_CREATE_SUCCESS,
-        PRODUCT_CREATE_FAIL,
-        PRODUCT_UPDATE_REQUEST,
-        PRODUCT_UPDATE_SUCCESS,
-        PRODUCT_UPDATE_FAIL,
-        PRODUCT_CREATE_REVIEW_SUCCESS,
-        PRODUCT_CREATE_REVIEW_FAIL,
-        PRODUCT_CREATE_REVIEW_REQUEST,
-        PRODUCT_TOP__REQUEST,
-        PRODUCT_TOP__SUCCESS,
-        PRODUCT_TOP__FAIL
-    } from '../constants/productConstants'
+import axios from "axios";
+import {
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+  PRODUCT_LIST_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
+  PRODUCT_DELETE_REQUEST,
+  PRODUCT_DELETE_SUCCESS,
+  PRODUCT_DELETE_FAIL,
+  PRODUCT_CREATE_REQUEST,
+  PRODUCT_CREATE_SUCCESS,
+  PRODUCT_CREATE_FAIL,
+  PRODUCT_UPDATE_REQUEST,
+  PRODUCT_UPDATE_SUCCESS,
+  PRODUCT_UPDATE_FAIL,
+  PRODUCT_CREATE_REVIEW_SUCCESS,
+  PRODUCT_CREATE_REVIEW_FAIL,
+  PRODUCT_CREATE_REVIEW_REQUEST,
+  PRODUCT_TOP__REQUEST,
+  PRODUCT_TOP__SUCCESS,
+  PRODUCT_TOP__FAIL,
+} from "../constants/productConstants";
 
-export const listProducts = (keyword = '', pageNumber = '') => async (dispatch) => {
+export const listProducts =
+  (keyword = "", pageNumber = "") =>
+  async (dispatch) => {
     try {
-        dispatch({ type: PRODUCT_LIST_REQUEST })
+      dispatch({ type: PRODUCT_LIST_REQUEST });
 
-        const { data } = await axios.get(
-          `/api/products?pageNumber=${pageNumber}&keyword=${keyword}`
-        )
+      const { data } = await axios.get(
+        `https://bintus-commerce.onrender.com/api/products?pageNumber=${pageNumber}&keyword=${keyword}`
+      );
 
-        dispatch({
-            type: PRODUCT_LIST_SUCCESS,
-            payload: data
-        })
+      dispatch({
+        type: PRODUCT_LIST_SUCCESS,
+        payload: data,
+      });
     } catch (error) {
-        dispatch({
-            type: PRODUCT_LIST_FAIL,
-            payload: error.response && error.response.data.message 
+      dispatch({
+        type: PRODUCT_LIST_FAIL,
+        payload:
+          error.response && error.response.data.message
             ? error.response.data.message
-            : error.message
-        })
+            : error.message,
+      });
     }
-}
+  };
 
 export const listProductDetails = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_DETAILS_REQUEST });
 
-    try {
-        dispatch({ type: PRODUCT_DETAILS_REQUEST })
+    const { data } = await axios.get(
+      `https://bintus-commerce.onrender.com/api/products/${id}`
+    );
 
-        const { data } = await axios.get(`/api/products/${id}`)
+    dispatch({
+      type: PRODUCT_DETAILS_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
 
-        dispatch({
-            type: PRODUCT_DETAILS_SUCCESS,
-            payload: data
-        })
-    } catch (error) {
-        dispatch({
-            type: PRODUCT_DETAILS_FAIL,
-            payload: error.response && error.response.data.message 
-            ? error.response.data.message
-            : error.message
-        })
-    }
-}
+export const deleteProduct = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: PRODUCT_DELETE_REQUEST,
+    });
 
-export const deleteProduct = (id)  => async (dispatch, getState) => {
-    try {
-        dispatch({
-            type: PRODUCT_DELETE_REQUEST,
-        })
+    const {
+      userLogin: { userInfo },
+    } = getState();
 
-        const { userLogin: { userInfo } } = getState()
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
 
-        const config = {
-            headers: {
-                Authorization: `Bearer ${userInfo.token}`
-            },
-        }
+    await axios.delete(
+      `https://bintus-commerce.onrender.com/api/products/${id}`,
+      config
+    );
 
-         await axios.delete(`/api/products/${id}`, config)
+    dispatch({
+      type: PRODUCT_DELETE_SUCCESS,
+    });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DELETE_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
 
-        dispatch({
-            type: PRODUCT_DELETE_SUCCESS,
-        })
-    } catch (error) {
-        dispatch({
-            type: PRODUCT_DELETE_FAIL,
-            payload: error.response && error.response.data.message 
-            ? error.response.data.message
-            : error.message
-        })
-    }
-}
+export const createProduct = () => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: PRODUCT_CREATE_REQUEST,
+    });
 
-export const createProduct = ()  => async (dispatch, getState) => {
-    try {
-        dispatch({
-            type: PRODUCT_CREATE_REQUEST,
-        })
+    const {
+      userLogin: { userInfo },
+    } = getState();
 
-        const { userLogin: { userInfo } } = getState()
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
 
-        const config = {
-            headers: {
-                Authorization: `Bearer ${userInfo.token}`
-            },
-        }
+    const { data } = await axios.post(
+      `https://bintus-commerce.onrender.com/api/products`,
+      {},
+      config
+    );
 
-         const { data } = await axios.post(`/api/products`, {}, config)
+    dispatch({
+      type: PRODUCT_CREATE_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_CREATE_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
 
-        dispatch({
-            type: PRODUCT_CREATE_SUCCESS,
-            payload: data
-        })
-    } catch (error) {
-        dispatch({
-            type: PRODUCT_CREATE_FAIL,
-            payload: error.response && error.response.data.message 
-            ? error.response.data.message
-            : error.message
-        })
-    }
-}
+export const updateProduct = (product) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: PRODUCT_UPDATE_REQUEST,
+    });
 
-export const updateProduct = (product)  => async (dispatch, getState) => {
-    try {
-        dispatch({
-            type: PRODUCT_UPDATE_REQUEST,
-        })
+    const {
+      userLogin: { userInfo },
+    } = getState();
 
-        const { userLogin: { userInfo } } = getState()
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
 
-        const config = {
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: `Bearer ${userInfo.token}`
-            },
-        }
+    const { data } = await axios.put(
+      `https://bintus-commerce.onrender.com/api/products/${product._id}`,
+      product,
+      config
+    );
 
-         const { data } = await axios.put(`/api/products/${product._id}`, product, config)
-
-        dispatch({
-            type: PRODUCT_UPDATE_SUCCESS,
-            payload: data
-        })
-    } catch (error) {
-        dispatch({
-            type: PRODUCT_UPDATE_FAIL,
-            payload: error.response && error.response.data.message 
-            ? error.response.data.message
-            : error.message
-        })
-    }
-}
+    dispatch({
+      type: PRODUCT_UPDATE_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_UPDATE_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
 
 // create reviews
 export const reviewProduct =
@@ -175,7 +200,11 @@ export const reviewProduct =
         },
       };
 
-      await axios.post(`/api/products/${productId}/reviews`, review, config);
+      await axios.post(
+        `https://bintus-commerce.onrender.com/api/products/${productId}/reviews`,
+        review,
+        config
+      );
 
       dispatch({
         type: PRODUCT_CREATE_REVIEW_SUCCESS,
@@ -189,23 +218,25 @@ export const reviewProduct =
             : error.message,
       });
     }
-  }
-  
-export const listTopProducts = () => async (dispatch) => {
-    try {
-      dispatch({ type: PRODUCT_TOP__REQUEST });
-      const { data } = await axios.get(`/api/products/top`);
-      dispatch({
-        type: PRODUCT_TOP__SUCCESS,
-        payload: data,
-      });
-    } catch (error) {
-      dispatch({
-        type: PRODUCT_TOP__FAIL,
-        payload:
-          error.response && error.response.data.message
-            ? error.response.data.message
-            : error.message,
-      });
-    }
   };
+
+export const listTopProducts = () => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_TOP__REQUEST });
+    const { data } = await axios.get(
+      `https://bintus-commerce.onrender.com/api/products/top`
+    );
+    dispatch({
+      type: PRODUCT_TOP__SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_TOP__FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
